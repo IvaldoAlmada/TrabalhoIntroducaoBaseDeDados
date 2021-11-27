968f186b24b7c2d9f6050d081f6951c0dc1f5949
Ivaldo Almada <ivaldoalmada@gmail.com>
2021-11-27 19:22:29 +0000

    add create table query

--- ddl.sql
+++ ddl.sql
@@ -1,3 +1,16 @@
+-- Database: find_supermarket
+
+-- DROP DATABASE IF EXISTS find_supermarket;
+
+CREATE DATABASE find_supermarket
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
 CREATE EXTENSION postgis;
 
 SELECT postgis_full_version();
